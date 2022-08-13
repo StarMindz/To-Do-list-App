7c6041b22cc6601025e4799dada547b5ee535dba
StarMindz <64619873+StarMindz@users.noreply.github.com>
2022-08-13 07:57:40 +0100

    Setup webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { mainModule } = require('process');
+// const { mainModule } = require('process');
 
 module.exports = {
   mode: 'development',
