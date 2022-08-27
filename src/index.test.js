89efb08482d0603956b76707aa1c4313c1830b3b
StarMindz <64619873+StarMindz@users.noreply.github.com>
2022-08-27 06:49:39 +0100

    Add test to clear and complted functions

--- src/index.test.js
+++ src/index.test.js
@@ -42,35 +42,35 @@ describe('Adjusting to the todo list', () => {
     expect(newTask.taskList.length).toBe(2);
   });
 });
+// Edith, please uncomment this part of the code below. And also dont forget to delete this line of comment. Thank you
+// // Test for Edit functions
+// describe('Edit a todo item', () => {
+//   const newTask = new Tasks();
+//   test('Edit an existing todo item', () => {
+//     // clear the list by making it innerHtml empty
+//     const list = document.getElementById('to-do-list');
+//     list.innerHTML = '';
 
-// Test for Edit functions
-describe('Edit a todo item', () => {
-  const newTask = new Tasks();
-  test('Edit an existing todo item', () => {
-    // clear the list by making it innerHtml empty
-    const list = document.getElementById('to-do-list');
-    list.innerHTML = '';
+//     // Add three new tasks
+//     newTask.addTask('First task');
+//     newTask.addTask('Second Task');
+//     newTask.addTask('Third task');
+//     // Target the second task with index '1'
+//     const second = document.querySelectorAll('.list-item')[1].children[1].children[0];
+//     // check that the value of the second element is as created
+//     expect(second.value).toBe('Second Task');
 
-    // Add three new tasks
-    newTask.addTask('First task');
-    newTask.addTask('Second Task');
-    newTask.addTask('Third task');
-    // Target the second task with index '1'
-    const second = document.querySelectorAll('.list-item')[1].children[1].children[0];
-    // check that the value of the second element is as created
-    expect(second.value).toBe('Second Task');
+//     // Edit the value
+//     second.value = 'Second Task Edited';
 
-    // Edit the value
-    second.value = 'Second Task Edited';
+//     // check that the value has changed as edited
+//     expect(second.value).toBe('Second Task Edited');
 
-    // check that the value has changed as edited
-    expect(second.value).toBe('Second Task Edited');
-
-    // Expect the number of list item to still be 1
-    const listItems = document.querySelectorAll('.list-item');
-    expect(listItems.length).toBe(3);
-  });
-});
+//     // Expect the number of list item to still be 1
+//     const listItems = document.querySelectorAll('.list-item');
+//     expect(listItems.length).toBe(3);
+//   });
+// });
 
 // Test for Clear functions
 
@@ -91,33 +91,33 @@ describe('Functions for updating completed item', () => {
     firstcheckbox.checked = true;
     expect(firstcheckbox.checked).toBe(true);
   });
+// Gleeny Uncomment this part of the code below and also delete this line of code. Thank you
+//   // Test to check that completed tasks are removed from the list
+//   test('Testing function for clearing all complted tasks', () => {
+//     const newTask = new Tasks();
+//     // clear the list by making it innerHtml empty
+//     const list = document.getElementById('to-do-list');
+//     list.innerHTML = '';
 
-  // Test to check that completed tasks are removed from the list
-  test('Testing function for clearing all complted tasks', () => {
-    const newTask = new Tasks();
-    // clear the list by making it innerHtml empty
-    const list = document.getElementById('to-do-list');
-    list.innerHTML = '';
+//     // Add Four new tasks
+//     newTask.addTask('First uncompleted');
+//     newTask.addTask('Second uncompleted');
+//     newTask.addTask('Third uncompleted');
+//     newTask.addTask('Fourth uncompleted');
 
-    // Add Four new tasks
-    newTask.addTask('First uncompleted');
-    newTask.addTask('Second uncompleted');
-    newTask.addTask('Third uncompleted');
-    newTask.addTask('Fourth uncompleted');
+//     expect(newTask.taskList.length).toBe(4);
 
-    expect(newTask.taskList.length).toBe(4);
+//     // Get first and third item checkbox
+//     const firstcheckbox = document.querySelectorAll('.list-item')[0].children[0];
+//     const thirdcheckbox = document.querySelectorAll('.list-item')[2].children[0];
 
-    // Get first and third item checkbox
-    const firstcheckbox = document.querySelectorAll('.list-item')[0].children[0];
-    const thirdcheckbox = document.querySelectorAll('.list-item')[2].children[0];
-
-    firstcheckbox.checked = true;
-    thirdcheckbox.checked = true;
-    newTask.taskList[0].completed = true;
-    newTask.taskList[2].completed = true;
-    newTask.taskList = clearAllFunction(newTask.taskList);
-    expect(firstcheckbox.checked).toBe(true);
-    expect(thirdcheckbox.checked).toBe(true);
-    expect(newTask.taskList.length).toBe(2);
-  });
-});
+//     firstcheckbox.checked = true;
+//     thirdcheckbox.checked = true;
+//     newTask.taskList[0].completed = true;
+//     newTask.taskList[2].completed = true;
+//     newTask.taskList = clearAllFunction(newTask.taskList);
+//     expect(firstcheckbox.checked).toBe(true);
+//     expect(thirdcheckbox.checked).toBe(true);
+//     expect(newTask.taskList.length).toBe(2);
+//   });
+// });
